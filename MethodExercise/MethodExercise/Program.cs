38cb6bb8c431f5e0294cbd074541de61951b06d2
Baines Deal <bainesdeal@gmail.com>
2022-06-01 15:31:46 -0500

    final MethodExercise commit

--- MethodExercise/MethodExercise/Program.cs
+++ MethodExercise/MethodExercise/Program.cs
@@ -4,22 +4,26 @@ namespace MethodExercise1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void MadLib()
         {
             Console.WriteLine("Hello! What is your name?"); // greeting
             var name = Console.ReadLine(); // user input 1
             Console.WriteLine($"Hello, {name}, I will tell you a story if you give me some infromation." +
-                "\nWhat is your favorite color?");
+                "\nWhat is your favorite color?"); // response to input 1
             var favColor = Console.ReadLine(); // user fav color
             Console.WriteLine($"Thanks, {name}, what about your favorite animal?");
-            var favAnimal = Console.ReadLine();
+            var favAnimal = Console.ReadLine(); // user fave animal
             Console.WriteLine("Lastly, what's your favorite band?");
-            var favBand = Console.ReadLine();
+            var favBand = Console.ReadLine(); //user fav band
             Console.WriteLine($"Great! here it goes!\nA long time ago a brave hero, named {name}, appeared in a " +
                 $"troubled, mystical land.\nOn a quest to save the land, {name} met a {favColor} {favAnimal}.\nTogether" +
                 $" they saved the world, and all the citizens of the land threw an amazing party " +
-                $"with live music performed by,\nyou guessed it, {favBand}!\nThe End!");
+                $"with live music performed by,\nyou guessed it, {favBand}!\n \nThe End!");
 
+        }
+        static void Main(string[] args)
+        {
+            MadLib();
 
         }
     }
---
+++ MethodExercise2/MethodExercise2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MethodExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+          {
+            int total1 = Sum(3, 100, 3, 5);
+            int difference1 = Subt(5, 2, 1);
+            int product1 = Multiply(2, 4, 2, 2);
+            int quotient1 = Div(16, 2, 2);
+
+            
+            Console.WriteLine(total1);
+            Console.WriteLine(difference1);
+            Console.WriteLine(product1);
+            Console.WriteLine(quotient1);
+
+        }
+
+        static public int Sum(params int[] sums1)
+        {
+           int total = 0;
+            foreach (int i in sums1)
+            {
+                total += i;
+            }
+            return total;
+        }
+        static public int Subt(params int[] subs1)
+        {
+            int difference = subs1[0] + subs1[0];
+            foreach( int num in subs1)
+            {
+                difference -= num;
+            }
+            return difference;
+        }
+        static public int Multiply(params int[] mults1)
+        {
+            int product = 1;
+            for(int i = 0; i < mults1.Length; i++)
+            { 
+                product *= mults1[i];
+            }
+            return product;
+        }
+        static public int Div(params int[] divs1)
+        {
+            int quotient = divs1[0] * divs1[0];
+            foreach(int num in divs1)
+            {
+                quotient /= num;
+            }
+            return quotient;
+        }
+
+        
+
+    }
+}
